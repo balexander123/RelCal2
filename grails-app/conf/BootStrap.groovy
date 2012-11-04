85410e4d935b2426bd9a1db1ec1a6dad7552cb17
Barry Alexander <barry.alexander@gmail.com>
2012-11-03 21:28:22 -0700

    added shiro

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,12 @@
+import org.apache.shiro.crypto.hash.Sha256Hash
+import com.gap.shiro.User
+
 class BootStrap {
 
     def init = { servletContext ->
+		def user = new User(username: "Admin", passwordHash: new Sha256Hash("Access4admin").toHex())
+		user.addToPermissions("*:*")
+		user.save()
     }
     def destroy = {
     }
