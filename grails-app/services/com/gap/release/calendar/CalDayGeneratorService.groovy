195feab1979230170a921a4a56f95ce7365d04d1
Barry Alexander <barry.alexander@gmail.com>
2012-12-10 21:54:44 -0800

    Switch to 2 week releases
    Two week releases now use YY.MM.n notation where n oscilates between 1
    and 2 as in 13.01.1 and 13.01.2 for releases in January 2013.

--- grails-app/services/com/gap/release/calendar/CalDayGeneratorService.groovy
+++ grails-app/services/com/gap/release/calendar/CalDayGeneratorService.groovy
@@ -8,24 +8,25 @@ class CalDayGeneratorService {
     def generateDays(Release release) {		
 		Integer totalDuration = 0
 		DateTime nextReleaseDate = new DateTime(release.getStartDate())
-		nextReleaseDate = nextReleaseDate.plus(Period.days(release.getRelDurationDays()*2))
+		nextReleaseDate = nextReleaseDate.plus(Period.days(release.getRelDurationDays()*release.getNumIterations()))
 		Date relCalDate = release.getStartDate()
 		Integer iterationNumber = release.getIterationNumber()
+		Integer relSuffix = 1
 		while (totalDuration < 365) {
 			Integer relCnt = 0
 		    while (relCnt < release.getRelDurationDays()) {
-				String relName = nextReleaseDate.toString()
-				relName = relName[2..3]
+				String relYearName = nextReleaseDate.toString()
+				relYearName = relYearName[2..3]
 				def relMonth = nextReleaseDate.monthOfYear.toString().padLeft(2,'0')
-				relName = relName + '.' + relMonth
+				String relPrefixName = relYearName + '.' + relMonth
 		
 				for (int iterationCnt=0; iterationCnt < release.getNumIterations(); iterationCnt++) {
 					int itDay=1
-					for (int iterationDays=0; iterationDays < (release.getRelDurationDays())/2; iterationDays++ ) {
+					for (int iterationDays=0; iterationDays < (release.getRelDurationDays())/release.getNumIterations(); iterationDays++ ) {
 						def day = new Day()
 						day.iterationNumber = iterationNumber
 						day.relCalDay = relCalDate
-						day.release = relName
+						day.release = relPrefixName + '.' + relSuffix.toString()
 						day.releaseId = release.id
 						if ((relCalDate[Calendar.DAY_OF_WEEK] != Calendar.SATURDAY) &&
 							(relCalDate[Calendar.DAY_OF_WEEK] != Calendar.SUNDAY)) {
@@ -38,6 +39,10 @@ class CalDayGeneratorService {
 						relCnt++
 					}
 					iterationNumber++
+					if (relSuffix == 1)
+						relSuffix = 2
+					else
+						relSuffix = 1
 				}
 		    }
 		    totalDuration = totalDuration + release.getRelDurationDays()
